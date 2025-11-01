08fbc5511096f637c5a3d84f0e66132a788877cb
I Wachtel <iwachtel73@gmail.com>
2025-11-01 11:26:49 +0100

    Initial Commit

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,9 @@
+package org.example.app
+
+import org.example.utils.Printer
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+}
