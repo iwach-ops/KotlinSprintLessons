6bcbcb291d37bb66c644431247536df1365d4e1c
I Wachtel <iwachtel73@gmail.com>
2025-11-02 14:23:38 +0100

    lesson 1 task1 variables

---
+++ app/src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -0,0 +1,10 @@
+package org.example.app.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val sellerRespond: String = "Thank you for your purchase!"
+
+    println("number of orders: $numberOfOrders")
+    println(sellerRespond)
+}
