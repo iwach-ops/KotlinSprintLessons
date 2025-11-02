a8b1526332c3ba31b26d70d3dd14c78c8d81aa1d
I Wachtel <iwachtel73@gmail.com>
2025-11-02 21:04:12 +0100

    lesson 1 task3 variables

---
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,23 @@
+package org.example.app.lesson_1
+
+fun main() {
+
+
+    val firstSpaceFlightYear: Int = 1961
+    var hour = 9
+    var minute = 7
+
+    println("first space flight year: $firstSpaceFlightYear")
+    println("Start flight hour: $hour")
+    println("Start flight minute:  $minute")
+
+    hour = 10
+    minute = 53
+
+    print("Stop flight time: ")
+    print(hour)
+    print(":")
+    print(minute)
+
+
+}
