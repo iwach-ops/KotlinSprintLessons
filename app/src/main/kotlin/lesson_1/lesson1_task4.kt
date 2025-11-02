6b96d09b0cfb742f487673c5d4013ef5abf58110
I Wachtel <iwachtel73@gmail.com>
2025-11-02 21:42:36 +0100

    lesson 1 task4 variables

---
+++ app/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,29 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val Distance: Long = 40868600000
+    val Age: Byte = 27
+    val partOfTheDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfTheYear: Double = 2.0547945205479453E-4
+    val apogeeOfTheOrbit: Int = 327000
+
+    print("Distance - ")
+    println(Distance)
+
+    print("Age - ")
+    println(Age)
+
+    print("partOfTheDay - ")
+    println(partOfTheDay)
+
+    print("seconds - ")
+    println(seconds)
+
+    print("partOfTheYear - ")
+    println(partOfTheYear)
+
+    print("apogeeOfTheOrbit - ")
+    println(apogeeOfTheOrbit)
+
+}
