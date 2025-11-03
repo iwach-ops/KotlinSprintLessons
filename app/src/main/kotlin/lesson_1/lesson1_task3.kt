d5155475fd977be35244174bb42c3e1c6d126c68
I Wachtel <iwachtel73@gmail.com>
2025-11-03 10:23:09 +0100

    lesson 1 task3 variables - fix type and space

--- app/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,23 +1,17 @@
 package org.example.app.lesson_1
 
 fun main() {
-
-
     val firstSpaceFlightYear: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println("first space flight year: $firstSpaceFlightYear")
     println("Start flight hour: $hour")
     println("Start flight minute:  $minute")
 
-    hour = 10
-    minute = 53
+    hour = "10"
+    minute = "55"
 
     print("Stop flight time: ")
-    print(hour)
-    print(":")
-    print(minute)
-
-
+    print("$hour:$minute")
 }
