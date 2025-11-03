864db481138af5b4567e6a617872ba21905acfdf
I Wachtel <iwachtel73@gmail.com>
2025-11-03 11:24:01 +0100

    lesson 1 task5 variables - add constants, delete spaces

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,14 @@
 package org.example.app.lesson_1
 
 fun main() {
+    val secondsInHour = 3600
+    val secondsInMinute = 60
     val totalSeconds: Int = 6480
 
-    val hours: Int = totalSeconds / 3600
-
-    val remainingSecondsAfterHours: Int = totalSeconds % 3600
-
-    val minutes: Int = remainingSecondsAfterHours /60
-
-    val seconds: Int = remainingSecondsAfterHours % 60
+    val hours: Int = totalSeconds / secondsInHour
+    val remainingSecondsAfterHours: Int = totalSeconds % secondsInHour
+    val minutes: Int = remainingSecondsAfterHours / secondsInMinute
+    val seconds: Int = remainingSecondsAfterHours % secondsInMinute
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
-
-
 }
