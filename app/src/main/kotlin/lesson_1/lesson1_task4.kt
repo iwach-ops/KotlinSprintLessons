eeb56695bf8a86a357d72a8c2170aac91539e825
I Wachtel <iwachtel73@gmail.com>
2025-11-03 10:48:38 +0100

    lesson 1 task4 variables - fix name of variables partially

--- app/src/main/kotlin/lesson_1/lesson1_task4.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,23 +1,23 @@
 package org.example.app.lesson_1
 
 fun main() {
-    val Distance: Long = 40868600000
-    val Age: Byte = 27
+    val distance: Long = 40868600000
+    val age: Byte = 27
     val partOfTheDay: Float = 0.075F
     val seconds: Short = 6480
     val partOfTheYear: Double = 2.0547945205479453E-4
     val apogeeOfTheOrbit: Int = 327000
 
     print("Distance - ")
-    println(Distance)
+    println(distance)
     print("Age - ")
-    println(Age)
-    print("partOfTheDay - ")
+    println(age)
+    print("Part of the day - ")
     println(partOfTheDay)
-    print("seconds - ")
+    print("Seconds - ")
     println(seconds)
-    print("partOfTheYear - ")
+    print("Part of the year - ")
     println(partOfTheYear)
-    print("apogeeOfTheOrbit - ")
+    print("Apogee of the orbit - ")
     println(apogeeOfTheOrbit)
 }
