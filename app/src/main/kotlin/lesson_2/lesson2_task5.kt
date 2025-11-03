64f6f01401553dfb23ab5110edbcac713561086a
I Wachtel <iwachtel73@gmail.com>
2025-11-03 22:11:11 +0100

    lesson 2 task5 - arith

---
+++ app/src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,16 @@
+package org.example.app.lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val finalAmount: Double
+    val initialDeposit: Double = 70000.0
+    val annualInterestRate: Double = 0.167 //16.7%
+    val numberOfTimesInterest: Byte = 1
+    val numberOfYears: Byte = 20
+
+    finalAmount =
+        (initialDeposit * (1 + (annualInterestRate / numberOfTimesInterest)).pow(numberOfTimesInterest * numberOfYears))
+
+    println("The final amount: %.3f".format(finalAmount))
+}
