005500f30931708911fe9c3ada3116b7c68cffa7
I Wachtel <iwachtel73@gmail.com>
2025-11-03 21:45:29 +0100

    lesson 1 task5 variables - fix const, number print format

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,16 @@
 package org.example.app.lesson_1
 
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
-    val secondsInHour = 3600
-    val secondsInMinute = 60
+
     val totalSeconds: Int = 6480
 
-    val hours: Int = totalSeconds / secondsInHour
-    val remainingSecondsAfterHours: Int = totalSeconds % secondsInHour
-    val minutes: Int = remainingSecondsAfterHours / secondsInMinute
-    val seconds: Int = remainingSecondsAfterHours % secondsInMinute
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val remainingSecondsAfterHours: Int = totalSeconds % SECONDS_IN_HOUR
+    val minutes: Int = remainingSecondsAfterHours / SECONDS_IN_MINUTE
+    val seconds: Int = remainingSecondsAfterHours % SECONDS_IN_MINUTE
 
-    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
