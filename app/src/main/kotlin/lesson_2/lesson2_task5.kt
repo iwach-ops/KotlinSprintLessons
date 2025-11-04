0f41638159a911d1854af8d8d5336b64b51c808d
I Wachtel <iwachtel73@gmail.com>
2025-11-04 17:56:27 +0100

    lesson 2 task5 - string - annualInterestRate init as percent

--- app/src/main/kotlin/lesson_2/lesson2_task5.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -5,12 +5,12 @@ import kotlin.math.pow
 fun main() {
     val finalAmount: Double
     val initialDeposit: Double = 70000.0
-    val annualInterestRate: Double = 0.167 //16.7%
+    val annualInterestRate: Double = 16.7
     val numberOfTimesInterest: Byte = 1
     val numberOfYears: Byte = 20
 
     finalAmount =
-        (initialDeposit * (1 + (annualInterestRate / numberOfTimesInterest)).pow(numberOfTimesInterest * numberOfYears))
+        (initialDeposit * (1 + ((annualInterestRate / 100) / numberOfTimesInterest)).pow(numberOfTimesInterest * numberOfYears))
 
     println("The final amount: %.3f".format(finalAmount))
 }
