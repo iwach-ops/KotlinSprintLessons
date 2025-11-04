9c0683424020fb93e4da35968c9639c046e621f6
I Wachtel <iwachtel73@gmail.com>
2025-11-04 18:09:52 +0100

    lesson 3 task2 - deleted additional age, surname

--- app/src/main/kotlin/lesson_3/lesson3_task2.kt
+++ app/src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -3,15 +3,11 @@ package org.example.app.lesson_3
 fun main() {
     val firstName: String = "Татьяна"
     val fatherName: String = "Сергеевна"
-    val surnameBeforeMarriage: String = "Андреева"
-    val surnameAfterMarriage: String = "Сидорова"
-    val ageBeforeMarriage: Byte = 20
-    val ageAfterMarriage: Byte = 22
+    var surname: String = "Андреева"
+    var age: Byte = 20
 
-    var age: Byte = ageBeforeMarriage
-    var surname: String = surnameBeforeMarriage
     println("$surname $firstName $fatherName, $age")
-    age = ageAfterMarriage
-    surname = surnameAfterMarriage
+    age = 22
+    surname = "Сидорова"
     println("$surname $firstName $fatherName, $age")
 }
