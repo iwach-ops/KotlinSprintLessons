62cca9580288d135adc8efb243fbf5135eae0450
I Wachtel <iwachtel73@gmail.com>
2025-11-05 21:45:55 +0100

    lesson 4 task4 - fix : without variables

--- app/src/main/kotlin/lesson_4/lesson4_task4.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,25 +1,14 @@
 package org.example.app.lesson_4
 
 fun main() {
-    var armsExercises: Boolean = true
-    var legsExercises: Boolean = false
-    var backExercises: Boolean = false
-    var pressExercises: Boolean = true
     val day: Int = 5
-
-    armsExercises = day % 2 != 0
-    legsExercises = day % 2 == 0
-    backExercises = day % 2 == 0
-    pressExercises = day % 2 != 0
 
     println(
         """
-             Упражнения для рук:    $armsExercises
-             Упражнения для ног:    $legsExercises
-             Упражнения для спины:  $backExercises
-             Упражнения для пресса: $pressExercises
+             Упражнения для рук:    ${day % 2 != 0}
+             Упражнения для ног:    ${day % 2 == 0}
+             Упражнения для спины:  ${day % 2 == 0}
+             Упражнения для пресса: ${day % 2 != 0}
         """.trimIndent()
     )
 }
-
-
