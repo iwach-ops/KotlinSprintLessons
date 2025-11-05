9a1a01e0daee5f34b30b52cc0d67cb425f5dec87
I Wachtel <iwachtel73@gmail.com>
2025-11-05 22:23:44 +0100

    lesson 4 task3 - fix : initialization

--- app/src/main/kotlin/lesson_4/lesson4_task3.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,18 @@
 package org.example.app.lesson_4
 
-fun main() {
-    var sunnyWeather: Boolean = true
-    var openedTent: Boolean = true
-    var airHumidity: Int = 20
-    var currentSeason: String = "winter"
+const val SUNNY_WEATHER = true
+const val OPENED_TENT = true
+const val AIR_HUMIDITY_CR = 20
+const val BAD_SEASON_CR = "зима"
 
-    var favorableConditions = (sunnyWeather && openedTent && airHumidity < 20 && currentSeason != "winter")
+fun main() {
+    val sunnyWeather = true
+    val openedTent = true
+    val airHumidity = 20
+    val currentSeason = "зима"
+
+    val favorableConditions =
+        (sunnyWeather == SUNNY_WEATHER && openedTent == OPENED_TENT && airHumidity == AIR_HUMIDITY_CR && currentSeason != BAD_SEASON_CR)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
