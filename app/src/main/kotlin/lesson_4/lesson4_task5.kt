a02f080a02cce09f322a2b7554862f6c8ba1fc20
I Wachtel <iwachtel73@gmail.com>
2025-11-06 21:15:44 +0100

    lesson 4 task5 - oper: fix add some variables to const

--- app/src/main/kotlin/lesson_4/lesson4_task5.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,11 @@
 package org.example.app.lesson_4
 
+const val MAX_CREW_SIZE = 70
+const val MIN_CREW_SIZE = 55
+const val CRATES_OF_PROVISIONS = 50
+val NO_DAMAGE_COND = true
+val FAVORABLE_WEATHER_COND = true
+
 fun main() {
     val noDamage = true
     val crewSize = 69
@@ -7,7 +13,7 @@ fun main() {
     val favorableWeather = true
 
     val shipCanSetSail =
-        (noDamage && crewSize >= 55 && crewSize <= 70 && cratesOfProvisions > 50) || (!noDamage && crewSize == 70 && cratesOfProvisions >= 50 && favorableWeather)
+        (noDamage==NO_DAMAGE_COND && crewSize >= MIN_CREW_SIZE && crewSize <= MAX_CREW_SIZE && cratesOfProvisions > CRATES_OF_PROVISIONS) || (noDamage!=NO_DAMAGE_COND && crewSize == MAX_CREW_SIZE && cratesOfProvisions >= CRATES_OF_PROVISIONS && favorableWeather==FAVORABLE_WEATHER_COND)
 
     println("Ship can set sail - Status: $shipCanSetSail")
 }
