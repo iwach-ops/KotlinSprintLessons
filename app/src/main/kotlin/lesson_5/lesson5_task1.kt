4369aaf06154a8be02ca1fda1dcbd638d2b483e7
I Wachtel <iwachtel73@gmail.com>
2025-11-07 14:22:19 +0100

    lesson 5 task1 : fix readln(), random

--- app/src/main/kotlin/lesson_5/lesson5_task1.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,14 +1,22 @@
 package org.example.app.lesson_5
 
+import kotlin.random.Random
+
 const val WELCOME_MESSAGE = "Добро пожаловать!"
 const val DENIED_MESSAGE = "Доступ запрещен."
-const val YOU_NOT_BOT_REQUEST = "Ты не бот?: 2+2 = "
-const val CALC_RESULT = 4
+const val YOU_NOT_BOT_REQUEST = "Ты не бот?"
+const val FROM_RANDOM = 1
+const val TO_RANDOM = 9
 
 fun main() {
-    print(YOU_NOT_BOT_REQUEST)
-    val result = readLine()?.toInt()
-    if (result == CALC_RESULT) {
+    val randomNumber1 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
+    val randomNumber2 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
+    val calcRes = randomNumber1 + randomNumber2
+
+    print("$YOU_NOT_BOT_REQUEST: $randomNumber1 + $randomNumber2 = ")
+    val result = readln().toInt()
+
+    if (result == calcRes) {
         println(WELCOME_MESSAGE)
     } else {
         println(DENIED_MESSAGE)
