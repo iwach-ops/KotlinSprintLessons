d122e159caa11159f74ce81e414d5ee3fbd1b54a
I Wachtel <iwachtel73@gmail.com>
2025-11-08 13:23:06 +0100

    lesson 5 task1 fix same const

--- app/src/main/kotlin/lesson_5/lesson5_task1.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -5,12 +5,12 @@ import kotlin.random.Random
 const val WELCOME_MESSAGE = "Добро пожаловать!"
 const val DENIED_MESSAGE = "Доступ запрещен."
 const val YOU_NOT_BOT_REQUEST = "Ты не бот?"
-const val FROM_RANDOM = 1
-const val TO_RANDOM = 9
+const val FROM_RANDOM_5_1 = 1
+const val TO_RANDOM_5_1 = 9
 
 fun main() {
-    val randomNumber1 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
-    val randomNumber2 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
+    val randomNumber1 = Random.nextInt(FROM_RANDOM_5_1, TO_RANDOM_5_1)
+    val randomNumber2 = Random.nextInt(FROM_RANDOM_5_1, TO_RANDOM_5_1)
     val calcRes = randomNumber1 + randomNumber2
 
     print("$YOU_NOT_BOT_REQUEST: $randomNumber1 + $randomNumber2 = ")
