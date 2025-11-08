962583548feffb8bc69e82e994bef2d479ef5f67
I Wachtel <iwachtel73@gmail.com>
2025-11-08 07:54:48 +0100

    lesson 5 task3 fix ().random(), delete a test, minimize if_string, output guessable numbers

--- app/src/main/kotlin/lesson_5/lesson5_task3.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,21 +1,19 @@
 package org.example.app.lesson_5
 
-import kotlin.random.Random
-
 const val GRAND_CONGRATULATION = "Congratulations! You guessed all numbers!"
 const val CONS_CONGRATULATION = "You guessed 1 number! You get a consolation prize!"
 const val UNLUCKY_ANSWER = "Sorry.. You lost!"
 const val IF_SAME_NUMBER_RESPONSE = "Unfortunately, you entered the same numbers. Game over!."
 const val GUESS_NUMBER_1_REQUEST = "Input a number1 (from 0 to 42): "
 const val GUESS_NUMBER_2_REQUEST = "Input a number2 (from 0 to 42): "
+const val GUESS_NUMBERS_RESPONSE = "Guessable numbers were:"
 const val FROM_RANDOM = 0
 const val TO_RANDOM = 42
 
 fun main() {
-    val guessRandomNumber1 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
-    val guessRandomNumber2 = Random.nextInt(FROM_RANDOM, TO_RANDOM)
+    val guessRandomNumber1 = (FROM_RANDOM..TO_RANDOM).random()
+    val guessRandomNumber2 = (FROM_RANDOM..TO_RANDOM).random()
 
-    println("Test: $guessRandomNumber1 & $guessRandomNumber2")
     print(GUESS_NUMBER_1_REQUEST)
     val inputNumber1 = readln().toInt()
     print(GUESS_NUMBER_2_REQUEST)
@@ -25,11 +23,34 @@ fun main() {
         println(IF_SAME_NUMBER_RESPONSE)
         return
     }
-    if ((inputNumber1 == guessRandomNumber1 || inputNumber1 == guessRandomNumber2) && (inputNumber2 == guessRandomNumber1 || inputNumber2 == guessRandomNumber2)) {
-        println(GRAND_CONGRATULATION)
-    } else if ((inputNumber1 == guessRandomNumber1 || inputNumber1 == guessRandomNumber2) || (inputNumber2 == guessRandomNumber1 || inputNumber2 == guessRandomNumber2)) {
-        println(CONS_CONGRATULATION)
+    if ((inputNumber1 == guessRandomNumber1 ||
+                inputNumber1 == guessRandomNumber2) &&
+        (inputNumber2 == guessRandomNumber1 ||
+                inputNumber2 == guessRandomNumber2)
+    ) {
+        println(
+            """
+            $GRAND_CONGRATULATION
+            $GUESS_NUMBERS_RESPONSE $guessRandomNumber1 $guessRandomNumber2
+        """.trimIndent()
+        )
+    } else if ((inputNumber1 == guessRandomNumber1 ||
+                inputNumber1 == guessRandomNumber2) ||
+        (inputNumber2 == guessRandomNumber1 ||
+                inputNumber2 == guessRandomNumber2)
+    ) {
+        println(
+            """
+            $CONS_CONGRATULATION
+            $GUESS_NUMBERS_RESPONSE $guessRandomNumber1 $guessRandomNumber2
+        """.trimIndent()
+        )
     } else {
-        println(UNLUCKY_ANSWER)
+        println(
+            """
+            $UNLUCKY_ANSWER
+            $GUESS_NUMBERS_RESPONSE $guessRandomNumber1 $guessRandomNumber2
+        """.trimIndent()
+        )
     }
 }
