204dbbceecba0778a40239c0f872d804f1f161c3
I Wachtel <iwachtel73@gmail.com>
2025-11-08 10:36:14 +0100

    lesson 5 task5 fix ().random(), DRY, when instead of if

--- app/src/main/kotlin/lesson_5/lesson5_task5.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,7 +1,5 @@
 package org.example.app.lesson_5
 
-import kotlin.random.Random
-
 const val GRAND_CONGRATULATION = "Congratulations! You guessed all 3 numbers! Jackpot!"
 const val BIG_GIFT_CONGRATULATION = "You guessed 2 numbers! You get a big prize."
 const val CONS_CONGRATULATION = "You guessed 1 number! You get a consolation prize!"
@@ -13,6 +11,7 @@ const val GUESS_NUMBER_1_REQUEST = "Input a number1 (from 0 to 42): "
 const val GUESS_NUMBER_2_REQUEST = "Input a number2 (from 0 to 42): "
 const val GUESS_NUMBER_3_REQUEST = "Input a number3 (from 0 to 42): "
 const val SAME_NUMBERS_ERROR = "Unfortunately, you entered the same numbers. Game over!"
+const val RANDOM_SAME_NUMBERS_ERROR = "Some of our random numbers matched. Restart our game please!"
 const val GUESS_NUMBERS_OUTPUT_RESPONSE = "They were guess numbers"
 const val NUMBER_OF_GUESS_OF_NUMBERS_3 = 3
 const val NUMBER_OF_GUESS_OF_NUMBERS_2 = 2
@@ -21,34 +20,40 @@ const val NUMBER_OF_GUESS_OF_NUMBERS_1 = 1
 fun main() {
     val inputNumberList = mutableListOf<Int>()
     val guessRandomNumbersList =
-        List(NUMBER_OF_GUESS_NUMBERS) { Random.nextInt(FIRST_GUESS_NUMBER_FROM_RANGE, LAST_GUESS_NUMBER_FROM_RANGE) }
+        List(NUMBER_OF_GUESS_NUMBERS) { (FIRST_GUESS_NUMBER_FROM_RANGE..LAST_GUESS_NUMBER_FROM_RANGE).random() }
 
-    println("Test: $GUESS_NUMBERS_OUTPUT_RESPONSE $guessRandomNumbersList")
+    if (guessRandomNumbersList.distinct().size != NUMBER_OF_GUESS_NUMBERS) {
+        println(RANDOM_SAME_NUMBERS_ERROR)
+        return
+    }
 
-    print(GUESS_NUMBER_1_REQUEST)
-    var inputNumber = readln().toInt()
-    inputNumberList.add(inputNumber)
-    print(GUESS_NUMBER_2_REQUEST)
-    inputNumber = readln().toInt()
-    inputNumberList.add(inputNumber)
-    print(GUESS_NUMBER_3_REQUEST)
-    inputNumber = readln().toInt()
-    inputNumberList.add(inputNumber)
+    val guessNumbersRequestList = listOf<String>(
+        GUESS_NUMBER_1_REQUEST, GUESS_NUMBER_2_REQUEST, GUESS_NUMBER_3_REQUEST
+    )
+
+    for (guessNumberRequest in guessNumbersRequestList) {
+        print(guessNumberRequest)
+        inputNumberList.add(readln().toInt())
+    }
 
     if (inputNumberList.distinct().size != NUMBER_OF_GUESS_NUMBERS) {
         println(SAME_NUMBERS_ERROR)
         return
     }
+
     val comparedNumbersList = inputNumberList.intersect(guessRandomNumbersList)
-    if (comparedNumbersList.size == NUMBER_OF_GUESS_OF_NUMBERS_3) {
-        println(GRAND_CONGRATULATION)
-    } else if (comparedNumbersList.size == NUMBER_OF_GUESS_OF_NUMBERS_2) {
-        println(BIG_GIFT_CONGRATULATION)
-    } else if (comparedNumbersList.size == NUMBER_OF_GUESS_OF_NUMBERS_1) {
-        println(CONS_CONGRATULATION)
-    } else {
-        println(UNLUCKY_ANSWER)
+
+    val resultMessage = when (comparedNumbersList.size) {
+        NUMBER_OF_GUESS_OF_NUMBERS_3 -> GRAND_CONGRATULATION
+        NUMBER_OF_GUESS_OF_NUMBERS_2 -> BIG_GIFT_CONGRATULATION
+        NUMBER_OF_GUESS_OF_NUMBERS_1 -> CONS_CONGRATULATION
+        else -> UNLUCKY_ANSWER
     }
 
-    println("$GUESS_NUMBERS_OUTPUT_RESPONSE $guessRandomNumbersList")
+    println(
+        """
+            $resultMessage
+            $GUESS_NUMBERS_OUTPUT_RESPONSE $guessRandomNumbersList
+        """.trimIndent()
+    )
 }
