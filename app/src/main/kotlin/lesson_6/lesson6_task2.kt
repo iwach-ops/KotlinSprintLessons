cadd5f226eee55da1932f1b81cf8e3b38ea6e3f6
I Wachtel <iwachtel73@gmail.com>
2025-11-10 16:20:29 +0100

    lesson 6 task2 wh fix without while

--- app/src/main/kotlin/lesson_6/lesson6_task2.kt
+++ app/src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -1,13 +1,12 @@
 package org.example.app.lesson_6
+
 const val TIMER_REQUEST = "How many seconds to count? "
-const val START_COMPARED_VALUE = 1
+const val SEC_IN_MSEC = 1000
 
 fun main() {
     print(TIMER_REQUEST)
     val counterN = readln().toInt()
-    var compareValue = START_COMPARED_VALUE
-    while (compareValue <= counterN) {
-        Thread.sleep(1000)
-        println("Counting , ${compareValue++} seconds")
-    }
+    val convertSecInMSec: Long = (counterN*SEC_IN_MSEC).toLong()
+    Thread.sleep(convertSecInMSec)
+    println("Counted , $counterN seconds")
 }
