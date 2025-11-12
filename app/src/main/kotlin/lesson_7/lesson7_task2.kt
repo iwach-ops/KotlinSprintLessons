2899217f1ae9357e1b94b182e5ab530c3176cd87
I Wachtel <iwachtel73@gmail.com>
2025-11-12 23:35:16 +0100

    lesson 7 task2 for - fix const name, while instead of for

--- app/src/main/kotlin/lesson_7/lesson7_task2.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -3,26 +3,24 @@ package org.example.app.lesson_7
 const val START_NUMB_RANGE = 1000
 const val STOP_NUMB_RANGE = 9999
 const val ACTIVATION_CODE_RESPONSE = "Your Activation Code: "
-const val Authorisation_OK_RESPONSE = "Authorisation successfully"
+const val AUTHORISATION_OK_RESPONSE = "Authorisation successfully"
 const val CODE_WRONG_ERROR = "Code is wrong"
 const val CODE_REQUEST = "Enter your code: "
 
 fun main() {
     val genericNumberRange = START_NUMB_RANGE..STOP_NUMB_RANGE
-    var genericCode = genericNumberRange.random()
-    println(ACTIVATION_CODE_RESPONSE + genericCode)
-
-    for (attempt in 1..Int.MAX_VALUE) {
+    var genericCode: Int
+    var inputCode: Int
+    do {
+        genericCode = genericNumberRange.random()
+        println(ACTIVATION_CODE_RESPONSE + genericCode)
         print(CODE_REQUEST)
-        val inputCode = readln().toInt()
+        inputCode = readln().toInt()
 
-        if (inputCode == genericCode) {
-            println(Authorisation_OK_RESPONSE)
-            return
-        } else {
+        if (inputCode != genericCode) {
             println(CODE_WRONG_ERROR)
-            genericCode = genericNumberRange.random()
-            println(ACTIVATION_CODE_RESPONSE + genericCode)
         }
-    }
+    } while (inputCode != genericCode)
+
+    println(AUTHORISATION_OK_RESPONSE)
 }
