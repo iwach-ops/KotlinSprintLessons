15d4d52337ccbc833d04cb148e2801038516a27c
I Wachtel <iwachtel73@gmail.com>
2025-11-13 21:04:32 +0100

    lesson 7 task3 fix start_index isn't const, string interpolation

--- app/src/main/kotlin/lesson_7/lesson7_task3.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task3.kt
@@ -1,13 +1,11 @@
 package org.example.app.lesson_7
 
-const val EVEN_NUMBER_RESPONSE = "even number: "
-const val START_INDEX = 0
+const val ENTER_NUMBER_REQUEST = "Enter number: "
+const val EVEN_NUMBER_RESPONSE = "Even number: "
 
 fun main() {
-    print("Enter Number: ")
+    print(ENTER_NUMBER_REQUEST)
     val inputNumber = readln().toInt()
 
-    for (counter in START_INDEX..inputNumber step (2)) {
-        println(EVEN_NUMBER_RESPONSE + counter)
-    }
+    for (counter in 0..inputNumber step (2)) println("$EVEN_NUMBER_RESPONSE $counter")
 }
