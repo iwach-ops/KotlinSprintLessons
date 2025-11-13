59966cd3d1005bdb8f089cab85928920d0cfee7f
I Wachtel <iwachtel73@gmail.com>
2025-11-13 21:40:39 +0100

    lesson 7 task5 fix range random, other password generation

--- app/src/main/kotlin/lesson_7/lesson7_task5.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,8 +1,7 @@
 package org.example.app.lesson_7
 
-const val FIRST_GEN_SET_POSITION = 1
-const val START_NUM_RANGE = 0
-const val STOP_NUM_RANGE = 9
+const val START_NUM_RANGE = '0'
+const val STOP_NUM_RANGE = '9'
 const val START_SYM_RANGE = 'a'
 const val STOP_SYM_RANGE = 'z'
 const val START_UP_SYMBOL_RANGE = 'A'
@@ -10,13 +9,13 @@ const val STOP_UP_SYMBOL_RANGE = 'Z'
 const val MIN_PASSWORD_SIZE = 6
 const val ENTER_PASS_REQUEST = "Enter password size: "
 const val PASSWORD_RESPONSE = "Password: "
-
 fun main() {
-    val genericNumberRange = START_NUM_RANGE..STOP_NUM_RANGE
-    val genericCharRange = START_SYM_RANGE..STOP_SYM_RANGE
-    val genericCharSymbol = START_UP_SYMBOL_RANGE..STOP_UP_SYMBOL_RANGE
-    val allGeneric = (genericNumberRange + genericCharRange + genericCharSymbol).toList()
-    var password = ""
+    val genericNumberCharRange = START_NUM_RANGE..STOP_NUM_RANGE
+    val genericLoCharRange = START_SYM_RANGE..STOP_SYM_RANGE
+    val genericUpCharSymbol = START_UP_SYMBOL_RANGE..STOP_UP_SYMBOL_RANGE
+    val passwordCharList = mutableListOf<Char>()
+
+    val allGeneric = genericNumberCharRange + genericLoCharRange + genericUpCharSymbol
 
     print(ENTER_PASS_REQUEST)
     var passwordSize = readln().toInt()
@@ -26,12 +25,14 @@ fun main() {
         passwordSize = readln().toInt()
     }
 
-    for (i in FIRST_GEN_SET_POSITION..passwordSize) {
-        val randomSymbol = allGeneric.random()
-        if (password.length < passwordSize) {
-            password += randomSymbol
-        }
-    }
+    passwordCharList += genericNumberCharRange.random()
+    passwordCharList += genericLoCharRange.random()
+    passwordCharList += genericUpCharSymbol.random()
 
-    println(PASSWORD_RESPONSE + password)
+    for (i in 4..passwordSize) passwordCharList += allGeneric.random()
+
+    passwordCharList.shuffle()
+    val password = passwordCharList.joinToString("")
+
+    println("$PASSWORD_RESPONSE $password")
 }
