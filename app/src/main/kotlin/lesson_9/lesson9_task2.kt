637d6897d8b0fed8ecd70eb8f22ce7d148730ab2
I Wachtel <iwachtel73@gmail.com>
2025-11-17 22:43:25 +0100

    lesson 9 task2

---
+++ app/src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,28 @@
+package org.example.app.lesson_9
+
+const val INGREDIENTS_RESPONSE = "The recipe contains the following base ingredients"
+const val APPROVE_ADD_NEW_INGREDIENT_REQUEST = "Would you like to add more? y/n: "
+const val ENTER_NEW_INGREDIENT = "Which ingredient do you want to add?: "
+const val PROGRAM_TERMINATED_RESPONSE = "Program is terminated!"
+const val NOW_FOLLOWING_INGREDIENTS_RESPONSE = "The recipe now contains the following ingredients"
+
+fun main() {
+    val pizzaIngredients = mutableListOf("tomato", "salt", "pepper")
+
+    println("$INGREDIENTS_RESPONSE ${pizzaIngredients.joinToString()}")
+
+    print(APPROVE_ADD_NEW_INGREDIENT_REQUEST)
+    val input = readln()
+    var newIngredient = ""
+
+    if (input.equals("y", true)) {
+        print(ENTER_NEW_INGREDIENT)
+        newIngredient = readln()
+    } else {
+        println(PROGRAM_TERMINATED_RESPONSE)
+        return
+    }
+
+    pizzaIngredients.add(newIngredient)
+    println("$NOW_FOLLOWING_INGREDIENTS_RESPONSE: ${pizzaIngredients.joinToString()}")
+}
