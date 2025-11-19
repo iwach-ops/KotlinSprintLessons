ab7a4b1048fb42491328b26366b469bc9b9b604a
I Wachtel <iwachtel73@gmail.com>
2025-11-19 21:32:21 +0100

    lesson 10 task1 fix when, rename function

--- app/src/main/kotlin/lesson_10/lesson10_task1.kt
+++ app/src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,7 +1,7 @@
 package org.example.app.lesson_10
 
-const val MOVE_DICE_HUMAN_REQUEST = "Please move, humanity...CLICK <ENTER>!"
-const val MOVE_DICE_COMPUTER = "Now Machine is moving dice..."
+const val ROLL_DICE_HUMAN_REQUEST = "Please roll, humanity...CLICK <ENTER>!"
+const val ROLL_DICE_COMPUTER = "Now Machine is rolling dice..."
 const val COMPUTER_PREPARATION_TIME = 2000L
 const val MAX_DICE_VALUE = 6
 const val HUMAN_RESULT_RESPONSE = "Humanity Result: "
@@ -11,21 +11,23 @@ const val MACHINE_WINS_RESPONSE = "The machine wins"
 const val FRIENDSHIP_WINS_RESPONSE = "Friendship wins"
 
 fun main() {
-    println(MOVE_DICE_HUMAN_REQUEST)
+    println(ROLL_DICE_HUMAN_REQUEST)
     readln()
 
-    val humanRes = moveDice(MAX_DICE_VALUE)
+    val humanRes = rollDice()
     println("$HUMAN_RESULT_RESPONSE $humanRes")
 
-    println(MOVE_DICE_COMPUTER)
+    println(ROLL_DICE_COMPUTER)
     Thread.sleep(COMPUTER_PREPARATION_TIME)
 
-    val compRes = moveDice(MAX_DICE_VALUE)
+    val compRes = rollDice()
     println("$MACHINE_RESULT_RESPONSE $compRes")
 
-    if (humanRes > compRes) println(HUMANITY_WINS_RESPONSE)
-    else if (humanRes < compRes) println(MACHINE_WINS_RESPONSE)
-    else println(FRIENDSHIP_WINS_RESPONSE)
+    when {
+        humanRes > compRes -> println(HUMANITY_WINS_RESPONSE)
+        humanRes < compRes -> println(MACHINE_WINS_RESPONSE)
+        else -> println(FRIENDSHIP_WINS_RESPONSE)
+    }
 }
 
-fun moveDice(maxDiceValue: Int): Int = (1..maxDiceValue).random()
+fun rollDice(): Int = (1..MAX_DICE_VALUE).random()
