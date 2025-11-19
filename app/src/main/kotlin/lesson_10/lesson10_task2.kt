da054761e9b7ec624ecaf9f561c11bfd1ccf6b04
I Wachtel <iwachtel73@gmail.com>
2025-11-19 21:25:21 +0100

    lesson 10 task2 fix welcome print_text, use const

--- app/src/main/kotlin/lesson_10/lesson10_task2.kt
+++ app/src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -2,6 +2,7 @@ package org.example.app.lesson_10
 
 const val MIN_LOGIN_PASSWORD_SIZE = 4
 const val WRONG_LONG_PASSWORD_LOGIN_RESPONSE = "The login or password is not long enough"
+const val PASSWORD_LOGIN_VALID = "Welcome! The login and password are valid"
 const val ENTER_YOUR_LOGIN_REQUEST = "Enter your login: "
 const val ENTER_YOUR_PASSWORD_REQUEST = "Enter your password: "
 
@@ -11,11 +12,8 @@ fun main() {
     print(ENTER_YOUR_PASSWORD_REQUEST)
     val password = readln()
 
-    if (!validateLoginPassword(login, MIN_LOGIN_PASSWORD_SIZE) ||
-        !validateLoginPassword(password, MIN_LOGIN_PASSWORD_SIZE)
-    ) {
-        println(WRONG_LONG_PASSWORD_LOGIN_RESPONSE)
-    }
+    if (!validateLoginPassword(login) || !validateLoginPassword(password)) println(WRONG_LONG_PASSWORD_LOGIN_RESPONSE)
+    else println(PASSWORD_LOGIN_VALID)
 }
 
-fun validateLoginPassword(passOrLogin: String, minSize: Int): Boolean = passOrLogin.length >= minSize
+fun validateLoginPassword(passOrLogin: String): Boolean = passOrLogin.length >= MIN_LOGIN_PASSWORD_SIZE
