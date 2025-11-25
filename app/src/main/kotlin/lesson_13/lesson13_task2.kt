1bfc3c47d8b91942a958494b69aebc20e5cd39d7
I Wachtel <iwachtel73@gmail.com>
2025-11-25 14:59:13 +0100

    lesson 13-2

---
+++ app/src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,18 @@
+package org.example.app.lesson_13
+
+fun main() {
+    val phoneDirect1 = PhoneDirect("Tom", 14331431134314)
+
+    phoneDirect1.printPhoneDirect()
+}
+
+class PhoneDirect(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null
+) {
+
+    fun printPhoneDirect() {
+        println("- Name: ${name}\n- Phone number: ${phoneNumber}\n- Company: ${company ?: "<not specified>"}")
+    }
+}
