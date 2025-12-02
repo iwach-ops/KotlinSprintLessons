d1e8df1d4e2a1be39b55a990ff1db4a61c5fb990
I Wachtel <iwachtel73@gmail.com>
2025-12-02 11:41:03 +0100

    lesson 14-4 fix name, forEach, listof in constructor

--- app/src/main/kotlin/lesson_14/KS-14-4.kt
+++ app/src/main/kotlin/lesson_14/KS-14-4.kt
@@ -1,39 +1,33 @@
 package org.example.app.lesson_14
 
 fun main() {
-
-    val planet = Planet("Mars", true, false)
     val sputnik1 = Sputnik("Phobos", true, false)
     val sputnik2 = Sputnik("Deimos", true, false)
 
-    planet.sputniks.add(sputnik1)
-    planet.sputniks.add(sputnik2)
+    val sputniks = listOf(sputnik1, sputnik2)
 
-    println(
-        """
-        ${planet.planetName} has sputniks:
-        ${sputnik1.sputnikName}
-        ${sputnik2.sputnikName}
-        
-    """.trimIndent()
-    )
+    val planet = Planet("Mars", true, false, sputniks)
+
+    println("${planet.name} has sputniks:")
+    planet.sputniks.forEach { sputnik ->
+        println(sputnik.name)
+    }
 }
 
 open class CelestialBodies(
     val haveAtmosphere: Boolean,
     val suitableForLanding: Boolean,
-
-    )
+)
 
 class Planet(
-    val planetName: String,
+    val name: String,
     haveAtmosphere: Boolean,
     suitableForLanding: Boolean,
-    val sputniks: MutableList<Sputnik> = mutableListOf(),
+    val sputniks: List<Sputnik>,
 ) : CelestialBodies(haveAtmosphere, suitableForLanding)
 
 class Sputnik(
-    val sputnikName: String,
+    val name: String,
     haveAtmosphere: Boolean,
     suitableForLanding: Boolean,
 ) : CelestialBodies(haveAtmosphere, suitableForLanding)
