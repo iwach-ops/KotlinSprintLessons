2dfbe0789b6b0cb4c56af690ae4381a6cf01182b
I Wachtel <iwachtel73@gmail.com>
2025-12-02 11:15:29 +0100

    lesson 14-3 fix color const, listof, val

--- app/src/main/kotlin/lesson_14/lesson14_task3.kt
+++ app/src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -2,39 +2,35 @@ package org.example.app.lesson_14
 
 import kotlin.math.PI
 
+const val COLOR_BLACK = "black"
+const val COLOR_WHITE = "white"
+
 fun main() {
-    val rect1 = Rectangle(4.0, 6.0, "black")
-    val circle1 = Circle(15.0, "black")
-    val rect2 = Rectangle(4.0, 6.0, "white")
-    val circle2 = Circle(15.0, "white")
-    val rect3 = Rectangle(4.0, 6.0, "white")
-    val circle3 = Circle(15.0, "white")
+    val rect1 = Rectangle(4.0, 6.0, COLOR_BLACK)
+    val circle1 = Circle(15.0, COLOR_BLACK)
+    val rect2 = Rectangle(4.0, 6.0, COLOR_WHITE)
+    val circle2 = Circle(15.0, COLOR_WHITE)
+    val rect3 = Rectangle(4.0, 6.0, COLOR_WHITE)
+    val circle3 = Circle(15.0, COLOR_WHITE)
 
-    val figures = mutableListOf<Figure>()
+    val figures = listOf(rect1, circle1, rect2, circle2, rect3, circle3)
 
-    figures.add(rect1)
-    figures.add(rect2)
-    figures.add(rect3)
-    figures.add(circle1)
-    figures.add(circle2)
-    figures.add(circle3)
-
-    val whiteFigures = figures.filter { it.color == "white" }
-    val blackFigures = figures.filter { it.color == "black" }
+    val whiteFigures = figures.filter { it.color == COLOR_WHITE }
+    val blackFigures = figures.filter { it.color == COLOR_BLACK }
 
     println(
         """
-        sum of white figures area: ${"%.2f".format(whiteFigures.sumOf { it.calculateArea()})}
-        sum of white figures perimeter: ${"%.2f".format(whiteFigures.sumOf { it.calculatePerimeter()})}
+        sum of white figures area: ${"%.2f".format(whiteFigures.sumOf { it.calculateArea() })}
+        sum of white figures perimeter: ${"%.2f".format(whiteFigures.sumOf { it.calculatePerimeter() })}
         sum of black figures area: ${"%.2f".format(blackFigures.sumOf { it.calculateArea() })}
-        sum of black figures perimeter: ${"%.2f".format(blackFigures.sumOf { it.calculatePerimeter()})}
+        sum of black figures perimeter: ${"%.2f".format(blackFigures.sumOf { it.calculatePerimeter() })}
         
     """.trimIndent()
     )
 }
 
 abstract class Figure(
-    var color: String
+    val color: String
 ) {
     abstract fun calculateArea(): Double
 
