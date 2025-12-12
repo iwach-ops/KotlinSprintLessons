396ce0139d9567353d97fa31a9368995bf0af979
I Wachtel <iwachtel73@gmail.com>
2025-12-12 16:55:24 +0100

    lesson 20-5

---
+++ app/src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -0,0 +1,32 @@
+package org.example.app.lesson_20
+
+fun main() {
+    val robot = Robot()
+
+    robot.say()
+
+    robot.setModifier { text -> text.uppercase() }
+
+    robot.say()
+}
+
+class Robot {
+
+    private var phrases: List<String> = listOf(
+        "Hi all",
+        "I am Robot",
+        "My name is Bob"
+    )
+
+    private var modifier: ((String) -> String)? = null
+
+    fun setModifier(modify: (String) -> String) {
+        modifier = modify
+    }
+
+    fun say() {
+        val phrase = phrases.random()
+        val result = modifier?.invoke(phrase) ?: phrase
+        println(result)
+    }
+}
