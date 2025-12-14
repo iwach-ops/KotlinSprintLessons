44862e6f2842decda08f9d60ce3cf3e86ad11dea
I Wachtel <iwachtel73@gmail.com>
2025-12-14 17:14:53 +0100

    lesson 20-4 fix list content

--- app/src/main/kotlin/lesson_20/lesson20_task4.kt
+++ app/src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -1,7 +1,7 @@
 package org.example.app.lesson_20
 
 fun main() {
-    val elements = listOf<String>("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight")
+    val elements = listOf<String>("Null", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight")
 
     val actions: List<() -> Unit> = elements.map { element -> { println("Element $element is pressed") } }
 
