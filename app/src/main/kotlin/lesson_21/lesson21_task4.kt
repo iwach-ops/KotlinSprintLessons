f8ce3a9c898d6f85199a574edda8884919501725
I Wachtel <iwachtel73@gmail.com>
2025-12-15 15:55:09 +0100

    lesson 21-4

---
+++ app/src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -0,0 +1,14 @@
+package org.example.app.lesson_21
+
+import java.io.File
+
+fun main() {
+    val file = File("words.txt")
+    file.writeWordToFile("HeLLO")
+}
+
+fun File.writeWordToFile(word: String) {
+    val currentText = if(exists()) readText() else ""
+    val lowText= word.lowercase()
+    writeText("$lowText\n$currentText")
+}
