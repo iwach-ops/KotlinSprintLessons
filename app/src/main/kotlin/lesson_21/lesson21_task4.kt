0ee80b140b8334dd3f6b67940197ba058fceecce
I Wachtel <iwachtel73@gmail.com>
2025-12-15 16:02:59 +0100

    lesson 21-4

--- app/src/main/kotlin/lesson_21/lesson21_task4.kt
+++ app/src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -4,11 +4,14 @@ import java.io.File
 
 fun main() {
     val file = File("words.txt")
+
     file.writeWordToFile("HeLLO")
 }
 
 fun File.writeWordToFile(word: String) {
     val currentText = if(exists()) readText() else ""
+
     val lowText= word.lowercase()
+
     writeText("$lowText\n$currentText")
 }
