f574be8975507e40437ffd755a33018c24454c53
I Wachtel <iwachtel73@gmail.com>
2025-12-17 14:36:45 +0100

    lesson 22-5

---
+++ app/src/main/kotlin/lesson_22/lesson22_task5.kt
@@ -0,0 +1,30 @@
+package org.example.app.lesson_22
+
+import java.time.LocalDateTime
+
+fun main() {
+    val alpha = GalacticGuide(
+        "Alpha Centauri",
+        "Alpha Centauri is the closest star system to our Solar System. It consists of three stars: Alpha Centauri A and Alpha Centauri B (which form a binary system) and Proxima Centauri, a small red dwarf. Proxima Centauri is the nearest individual star to the Sun and is known to have at least one planet in its habitable zone",
+        LocalDateTime.now(),
+        4.37
+    )
+
+    val (name, description, dateTime, distanceFromEarth) = alpha
+
+    println(
+        """
+        $name
+        $description
+        $dateTime
+        $distanceFromEarth        
+    """.trimIndent()
+    )
+}
+
+data class GalacticGuide(
+    val name: String,
+    val description: String,
+    val dateTime: LocalDateTime,
+    val distanceFromEarth: Double,
+)
